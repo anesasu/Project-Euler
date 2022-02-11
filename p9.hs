a2da4e233f28e303a439eff33fcd0c3394294fd2
anesasu <anesasu@protonmail.com>
2022-02-12 04:25:41 +1300

    Add p9.hs

---
+++ p9.hs
@@ -0,0 +1,13 @@
+module Main where
+
+
+import Numeric ( showFFloat )
+
+
+answer :: Int
+answer = round $ head [a*b*(1000-(a+b)) | a <- [1..998], b <- [1..999-a], a^^2+b^^2==(1000-(a+b))^^2]
+
+
+main :: IO ()
+main = do
+    print answer
